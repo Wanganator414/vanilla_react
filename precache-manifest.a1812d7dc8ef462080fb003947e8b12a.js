d7ed6edce953a9e219468fe5d2399b642abd3733
Wanganator <ewang414@gmail.com>
2019-08-18 09:45:19 +0800

    Updates

---
+++ precache-manifest.a1812d7dc8ef462080fb003947e8b12a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8817cb3dee2a481f5db4777d48c9e07c",
+    "url": "/vanilla_react/index.html"
+  },
+  {
+    "revision": "08df15af1e448aae11f2",
+    "url": "/vanilla_react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "00a1ad538d9052585961",
+    "url": "/vanilla_react/static/js/2.6cef45fb.chunk.js"
+  },
+  {
+    "revision": "08df15af1e448aae11f2",
+    "url": "/vanilla_react/static/js/main.d41a1234.chunk.js"
+  },
+  {
+    "revision": "07e1298d21af091b3427",
+    "url": "/vanilla_react/static/js/runtime~main.78427566.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vanilla_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
